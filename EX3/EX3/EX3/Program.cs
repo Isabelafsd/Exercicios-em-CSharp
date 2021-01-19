8cb44d9c6f96b2fe783f8131f201f58fc99e012d
Isabelafsd <71847131+Isabelafsd@users.noreply.github.com>
2021-01-19 18:55:13 -0300

    Add files via upload

---
+++ EX3/EX3/EX3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace EX3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			    Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("--------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exercicio 3!");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------");
+			    Console.ForegroundColor = ConsoleColor.White;
+            bool sair = false;
+            double salario;
+            while (!sair)
+            {
+                Console.Write("Digite o valor do salario: ");
+                try
+                {
+                    salario = double.Parse(Console.ReadLine());
+                    if (salario < 0)
+                        throw new FormatException();
+                    Console.WriteLine(AumentoSalario(salario));
+                    sair = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Salario invalido,digite uma tecla para continuar");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            finalizaPrograma();
+        }
+
+        public static string AumentoSalario(double salario)
+        {
+            double aumento;
+            int percent;
+            if (salario <= 1200)
+            {
+                percent = 10;
+                aumento = (percent * salario) / 100;
+                return "O salário antigo era de R$" + salario + " e deverar ter um aumento de R$" + aumento
+                + "(" + percent + "%).O novo salario será de R$" + (salario + aumento);
+            }
+            else
+            {
+                percent = 5;
+                aumento = (percent * salario) / 100;
+                return "O salário antigo era de R$" + salario + " e deverar ter um aumento de R$" + aumento
+                + "(" + percent + "%).O novo salario será de R$" + (salario + aumento);
+            }
+        }
+        public static void finalizaPrograma()
+        {
+            Console.WriteLine("--------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Programa finalizado!");
+            Console.WriteLine("Digite uma tecla para continuar");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ EX4/EX4/EX4/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			    Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exercício 4!");
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+			    Console.ForegroundColor = ConsoleColor.White;
+            int opcao;
+            double num1 = 0, num2 = 0;
+            bool erro = false, sair = false;
+            while (!sair)
+            {
+                menu();
+                try
+                {
+                    Console.Write("Opcão desejada:");
+                    opcao = int.Parse(Console.ReadLine());
+                    if (opcao > 2 || opcao < 1)
+                        throw new FormatException();
+                    else if (opcao == 1)
+                    {
+                        while (!erro)
+                        {
+                            Console.Write("Operando 1: ");
+                            erro = double.TryParse(Console.ReadLine(), out num1);
+                            if (!erro)
+                                erroEntrada();
+                            else
+                            {
+                                Console.Write("Operando 2: ");
+                                erro = double.TryParse(Console.ReadLine(), out num2);
+                                if (!erro)
+                                    erroEntrada();
+                            }
+                        }
+                        Console.WriteLine("O resultado da soma é: " + somaNumeros(num1, num2));
+                    }
+                    else
+                    {
+                        while (!erro)
+                        {
+                            Console.Write("Numero a ser calculado a raiz quadrada: ");
+                            erro = double.TryParse(Console.ReadLine(), out num1);
+                            if (!erro)
+                                erroEntrada();
+                        }
+                        Console.WriteLine("A raiz quadrada de " + num1 + " é " + raizQuadrada(num1));
+                    }
+                    sair = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Você deve digitar uma opção válida(1 ou 2)");
+                    Console.WriteLine("Digite uma tecla para continuar");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            finalizaPrograma();
+        }
+        public static void menu()
+        {
+            Console.WriteLine("Menu de opçoēs:\n1-Soma de dois números\n2-Raiz quadrada" +
+            " de um número");
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+        }
+        public static void erroEntrada()
+        {
+            Console.WriteLine("Numero inválido,digite uma tecla para continuar.");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static double somaNumeros(double n1, double n2)
+        {
+            return (n1 + n2);
+        }
+        public static double raizQuadrada(double num)
+        {
+            return Math.Sqrt(num);
+        }
+        public static void finalizaPrograma()
+        {
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            Console.WriteLine("Programa finalizado!");
+            Console.WriteLine("Digite uma tecla para fechar.");
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            Console.ReadKey();
+
+        }
+    }
+}
